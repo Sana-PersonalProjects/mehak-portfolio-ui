43402cad59571376b4ba73d2f3fde8dc0075365b
sdiwan <sdiwan@eshopworld.com>
2020-06-13 09:31:45 +0100

    add infrastructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mehak-portfolio-ui';
+export class AppComponent implements OnDestroy, OnInit{
+  tearDown$ = new Subject();
+
+  loading: boolean;
+
+  ngOnInit(): void {
+    this.loading = false;
+  }
+  ngOnDestroy(): void {
+    this.tearDown$.next();
+    this.tearDown$.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkeletonComponent } from './skeleton/skeleton.component';
+import { GalleryComponent } from './portfolio/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SkeletonComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
